af3f804717927d3aee1d3e3222ab936b71dfb72e
cksgh901 <83771269+cksgh901@users.noreply.github.com>
2021-05-07 16:53:40 +0900

    Add files via upload

---
+++ test/src/oo/Bmi.java
@@ -0,0 +1,27 @@
+package oo;
+
+public class Bmi {
+	 
+public static void main(String[] args) {
+ 
+double height = 180;
+double weight = 80;
+// 메서드 호출 및 출력
+System.out.println(bmi(height, weight));
+}
+
+ public static double bmi(double height, double weight) {
+	 double bmi=weight/(height/100*height/100);
+	
+	if(bmi>25) {
+		System.out.println("비만");
+	}else if(bmi>18.5) {
+	System.out.println("표준");
+	}else if(bmi<18.5) {
+		System.out.println("저체중");
+	}
+	return bmi; 
+}
+}
+
+
